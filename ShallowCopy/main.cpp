a40d8e53bb6a09b3fefd17c0864d639a39e09231
JisanulHaque <jisanulhaque1996@gmail.com>
2023-07-01 22:08:54 +0600

    ShallowCopy modified and debugged to understand the problem of shallow copy when a raw pointer is present

--- ShallowCopy/main.cpp
+++ ShallowCopy/main.cpp
@@ -18,8 +18,29 @@ Shallow(const Shallow &source);
 
 };
 
+Shallow::Shallow(int d){
+	data = new int;
+	*data = d;
+}
+
+Shallow::Shallow(const Shallow &source)
+	:data(source.data) {
+	cout<<"Copy Constructor-Shallow Copy"<<endl;
+}
+Shallow::~Shallow(){
+	delete data;
+	cout<<"Desconstructor freeing data"<<endl;
+}
+
+void display_shallow(Shallow s){
+	cout<<s.get_data_value()<<endl;
+}
 int main(){
+	Shallow obj1{100};
+	display_shallow(obj1);
 	
+	Shallow obj2{obj1};
+	obj2.set_data(1000);
 	
 	
 	return 0;
