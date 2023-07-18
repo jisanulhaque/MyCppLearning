547e088fa43923657eae117b31813385dc1000c2
JisanulHaque <jisanulhaque1996@gmail.com>
2023-07-18 11:40:41 +0600

    Practiced static class data and functions, how and why to use it

---
+++ StaticClassMembers/Player.h
@@ -0,0 +1,29 @@
+#ifndef _PLAYER_H_
+#define _PLAYER_H_
+#include <string>
+
+using namespace std;
+
+class Player
+{
+	static int num_players; //counts the active player objects
+	string name;
+	int health;
+	int xp;
+	
+public:
+	string get_name() {return name;}
+	int get_health(){return health;}
+	int get_xp(){return xp;}
+
+	Player(string name_val="None", int health_val=0, int xp_val=0);
+	//Copy Constructor
+	Player(const Player &source);
+	//Destructor
+	~Player();
+	
+	static int get_num_players();
+	
+};
+
+#endif //_PLAYER_H_
